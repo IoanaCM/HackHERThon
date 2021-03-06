8e8fd28c7b75574ee021469fe639ab546445a5e1
Ioana <ioanamihailecu02@yahoo.com>
2021-03-06 14:22:15 +0000

    Bare bones

--- app/src/main/java/com/example/hackherthon/MainActivity.java
+++ app/src/main/java/com/example/hackherthon/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.hackherthon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button isolating = (Button) findViewById(R.id.isolatingButton);
+
+        //add the onClick listener
+        isolating.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent isolatingIntent = new Intent(MainActivity.this, IsolatingMainActivity.class);
+
+                startActivity(isolatingIntent);
+            }
+        });
     }
+
+
 }
