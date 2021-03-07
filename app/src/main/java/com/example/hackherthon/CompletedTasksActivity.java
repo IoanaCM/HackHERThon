fc524d37deac75ba861668176cc5b20160396529
Dyuti Chakraborty <dc420@imperial.ac.uk>
2021-03-07 08:55:04 +0000

    style changes

--- app/src/main/java/com/example/hackherthon/CompletedTasksActivity.java
+++ app/src/main/java/com/example/hackherthon/CompletedTasksActivity.java
@@ -14,82 +14,10 @@ import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 public class CompletedTasksActivity extends AppCompatActivity {
-/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.completed_tasks);
-    }
-*/
-
-    public class ScreenSlidePageFragment extends Fragment {
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return (ViewGroup) inflater.inflate(
-                    R.layout.completed_task_fragment, container, false);
-        }
-    }
-
-    public class ScreenSlidePagerActivity extends FragmentActivity {
-        /**
-         * The number of pages (wizard steps) to show in this demo.
-         */
-        private static final int NUM_PAGES = 3;
-
-        /**
-         * The pager widget, which handles animation and allows swiping horizontally to access previous
-         * and next wizard steps.
-         */
-        private ViewPager2 viewPager;
-
-        /**
-         * The pager adapter, which provides the pages to the view pager widget.
-         */
-        private FragmentStateAdapter pagerAdapter;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.completed_tasks);
-
-            // Instantiate a ViewPager2 and a PagerAdapter.
-            viewPager = findViewById(R.id.pager);
-            pagerAdapter = new ScreenSlidePagerAdapter(this);
-            viewPager.setAdapter(pagerAdapter);
-        }
-
-        @Override
-        public void onBackPressed() {
-            if (viewPager.getCurrentItem() == 0) {
-                // If the user is currently looking at the first step, allow the system to handle the
-                // Back button. This calls finish() on this activity and pops the back stack.
-                super.onBackPressed();
-            } else {
-                // Otherwise, select the previous step.
-                viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-            }
-        }
-
-        /**
-         * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-         * sequence.
-         */
-        private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
-            public ScreenSlidePagerAdapter(FragmentActivity fa) {
-                super(fa);
-            }
-
-            @Override
-            public Fragment createFragment(int position) {
-                return new ScreenSlidePageFragment();
-            }
-
-            @Override
-            public int getItemCount() {
-                return NUM_PAGES;
-            }
-        }
     }
 
 }
