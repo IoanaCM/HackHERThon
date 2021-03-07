d40b4ce091e7be2d279549213c8f92c31288dc18
Athena Yim <athenayimws@gmail.com>
2021-03-07 11:14:19 +0000

    Add navigation bar and settings button to VolunteerCompletedTasks

---
+++ app/src/main/java/com/example/hackherthon/VolunteerCompletedTasksActivity.java
@@ -0,0 +1,55 @@
+package com.example.hackherthon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class VolunteerCompletedTasksActivity extends AppCompatActivity {
+
+    private BottomNavigationView navView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.completed_tasks);
+
+        ImageButton settingsButton = (ImageButton) findViewById(R.id.volunteer_settings);
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(VolunteerCompletedTasksActivity.this, VolunteerSettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        navView = (BottomNavigationView) findViewById(R.id.volunteer_bottom_navigation);
+        navView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.volunteer_searchtask:
+                                Intent searchIntent = new Intent(VolunteerCompletedTasksActivity.this, VolunteerSearchActivity.class);
+                                break;
+
+                            case R.id.volunteer_pendingtasks:
+                                Intent pendingIntent = new Intent(VolunteerCompletedTasksActivity.this, VolunteerMainActivity.class);
+                                startActivity(pendingIntent);
+                                break;
+
+                            case R.id.volunteer_completedtasks:
+                                break;
+                        }
+                        return false;
+                    }
+                });
+    }
+
+}
